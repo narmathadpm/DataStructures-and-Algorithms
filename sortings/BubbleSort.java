f6e88cba2420cb6e01707cffe5a1de38c2c11133
narmatha.d <narmatha.d@zohocorp.com>
2022-07-29 11:29:38 +0530

    Bubble Sort - algo and coding

---
+++ sortings/BubbleSort.java
@@ -0,0 +1,67 @@
+/*
+
+
+Bubble sort starts with very first two elements, comparing them to check which one is greater.
+( 5 1 4 2 8 ) –> ( 1 5 4 2 8 ), Here, algorithm compares the first two elements, and swaps since 5 > 1.
+( 1 5 4 2 8 ) –>  ( 1 4 5 2 8 ), Swap since 5 > 4
+( 1 4 5 2 8 ) –>  ( 1 4 2 5 8 ), Swap since 5 > 2
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ), Now, since these elements are already in order (8 > 5), algorithm does not swap them.
+(largest element will reach end of the array first
+
+Second Pass:
+
+Now, during second iteration it should look like this:
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 )
+( 1 4 2 5 8 ) –> ( 1 2 4 5 8 ), Swap since 4 > 2
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+( 1 2 4 5 8 ) –>  ( 1 2 4 5 8 )
+
+
+
+
+
+*/
+import java.util.Scanner;
+class BubbleSort {
+    public static void main(String[] args) {
+        //Scanner is used to get input from user
+        Scanner sc=new Scanner(System.in);
+        //No of elements in unsorted array
+        System.out.println("Enter array size");
+        int noOfElements =sc.nextInt();
+        //Declaring array and getting
+        System.out.println("Enter array elements");
+        int nums[] = new int[noOfElements];
+        for (int i=0;i<noOfElements;i++)
+        {
+            nums[i]=sc.nextInt();
+        }
+
+        //Bubble sort will compare two element and largest element to last position.
+
+        //It will have time complexity is O(n^2)
+        // not suitable for large problems
+
+        int temp=0;// To store element while swapping
+        for (int i=0;i<noOfElements;i++)
+        {
+            for(int j=0;j<noOfElements-i-1;j++)
+            {
+                if(nums[j]>nums[j+1])
+                {
+                    temp=nums[j+1];
+                    nums[j+1]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+
+        }
+
+        // printing sorted elements
+
+        for (int i=0;i<noOfElements;i++)
+        {
+            System.out.print(nums[i]+" ");
+        }
+    }
+}
