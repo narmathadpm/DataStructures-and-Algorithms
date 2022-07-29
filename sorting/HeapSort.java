a11938e91f939bfb3051a937c73f5c78200e9e2f
narmatha.d <narmatha.d@zohocorp.com>
2022-07-29 21:58:16 +0530

    heap sort

--- sorting/HeapSort.java
+++ sorting/HeapSort.java
@@ -64,6 +64,46 @@ Space Complexity	O(1)
 import java.util.Scanner;
 
 public class HeapSort {
+
+
+    void heapify(int[] nums,int n,int i)
+    {
+        int largest=i;
+        int left=2*i +1;
+        int right=2*i+2;
+        if(left<n&&nums[left]>nums[largest])
+        {
+            largest=left;
+        }
+        if(right<n&&nums[right]>nums[largest])
+        {
+            largest=right;
+        }
+        if(largest!=i)
+        {
+            int temp = nums[i];
+            nums[i] = nums[largest];
+            nums[largest] = temp;
+            heapify(nums, n, largest);
+        }
+    }
+    void sort(int[] nums,int n)
+    {
+        for(int i=n/2-1;i>=0;i--)
+        {
+           heapify(nums,n,i);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            /* Move current root element to end*/
+            // swap a[0] with a[i]
+            int temp = nums[0];
+            nums[0] = nums[i];
+            nums[i] = temp;
+
+            heapify(nums, i, 0);
+        }
+    }
     public static void main(String[] args) {
         //Scanner is used to get input from user
         Scanner sc = new Scanner(System.in);
@@ -76,8 +116,8 @@ public class HeapSort {
         for (int i = 0; i < noOfElements; i++) {
             nums[i] = sc.nextInt();
         }
-        QuickSort q = new QuickSort();
-        q.sort(nums, 0, noOfElements - 1);
+        HeapSort h = new HeapSort();
+        h.sort(nums,  noOfElements );
 
         for (int i = 0; i < noOfElements; i++) {
             System.out.print(nums[i] + " ");
